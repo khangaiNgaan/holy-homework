cfbc99b13dcf42c399d861a0d23d401aafbbf9fd
khangaiNgaan <salmon.caffeine@gmail.com>
2026-04-17 10:54:33 +0800

    py chap5 wip

---
+++ holy-python/chap5/5-1.py
@@ -0,0 +1,12 @@
+def count_char(s):
+    char_dict = {}
+    for i in range(len(s)):
+        if s[i].isalpha:
+            if s[i] not in char_dict:
+                char_dict[s[i]] = 0
+            char_dict[s[i]] += 1
+    for key, value
+def main():
+    s = input("请输入一个字符串: ")
+    s = s.lower()
+    count_char(s)
